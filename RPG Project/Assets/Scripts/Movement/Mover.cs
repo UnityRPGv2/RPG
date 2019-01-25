432207ff8c40a0a704ee7e0cd9a604cbed093778
Samuel Pattuzzi <sam@pattuzzi.it>
2019-01-25 13:00:48 +0000

    Pathfinding With NavMeshAgent
    + Put mover script in namespace for section
    + Move to target object each frame
    + Show moving the object around

---
+++ RPG Project/Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        void Update()
+        {
+            GetComponent<NavMeshAgent>().destination = target.position;
+        }
+    }
+}
