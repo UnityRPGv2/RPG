06679430ba69c53286e2fdc0016684c81fd2fcaf
Samuel Pattuzzi <sam@pattuzzi.it>
2019-01-26 14:58:07 +0000

    Creating A Fixed Follow Camera
    + Place follow camera root at player
    + Camera is child of this
    + Align camera with view
    + Follow script tracks position not rotation

---
+++ RPG Project/Assets/Scripts/Movement/FollowCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Movement
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        void Update()
+        {
+            transform.position = target.position;
+        }
+    }
+}
