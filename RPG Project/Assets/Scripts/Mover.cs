2f5e8f9d1c8035dd35fe94c373bdfd1e36474996
Rick <rickinaction@gmail.com>
2019-02-12 17:05:12 +1000

    3 Move Using Nav Mesh Agent

---
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
