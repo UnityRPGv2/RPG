dca07e4fec2f476a68568ac5c2d293defd27f635
Rick <rickinaction@gmail.com>
2019-02-25 11:30:12 +1000

    10 Basic Movement Tweaks

--- RPG Project/Assets/Scripts/FollowCamera.cs
+++ RPG Project/Assets/Scripts/FollowCamera.cs
@@ -6,7 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     [SerializeField] Transform target;
 
-    void Update()
+    void LateUpdate()
     {
         transform.position = target.position;
     }
--- RPG Project/Assets/Scripts/Mover.cs
+++ RPG Project/Assets/Scripts/Mover.cs
@@ -9,7 +9,7 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursor();
         }
