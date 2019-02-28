e5d285aed9c641bff884015c18a555e13a6a90d6
Samuel Pattuzzi <sam@pattuzzi.it>
2019-02-28 09:51:12 +0000

    2 Separating The Control Layer

---
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private void Update() {
+        if (Input.GetMouseButton(0))
+        {
+            MoveToCursor();
+        }
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+        if (hasHit)
+        {
+            GetComponent<Mover>().MoveTo(hit.point);
+        }
+    }
+}
--- RPG Project/Assets/Scripts/Movement/Mover.cs
+++ RPG Project/Assets/Scripts/Movement/Mover.cs
@@ -9,23 +9,14 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            MoveToCursor();
-        }
         UpdateAnimator();
     }
 
-    private void MoveToCursor()
+    public void MoveTo(Vector3 destination)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
-        {
-            GetComponent<NavMeshAgent>().destination = hit.point;
-        }
+        GetComponent<NavMeshAgent>().destination = destination;
     }
+
     private void UpdateAnimator()
     {
         Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
