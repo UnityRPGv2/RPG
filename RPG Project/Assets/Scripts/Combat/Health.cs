c425379d4d3fad594cbf9ff57edb7f1bdf58a940
Samuel Pattuzzi <sam@pattuzzi.it>
2019-03-03 20:29:46 +0000

    Trigger Death Animation Only Once

--- RPG Project/Assets/Scripts/Combat/Health.cs
+++ RPG Project/Assets/Scripts/Combat/Health.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -5,10 +6,23 @@ namespace RPG.Combat
     public class Health : MonoBehaviour {
         [SerializeField] float healthPoints = 100;
 
+        bool isDead = false;
+
         public void TakeDamage(float damage)
         {
             healthPoints = Mathf.Max(0, healthPoints - damage);
-            print(healthPoints);
+            if (healthPoints == 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
         }
     }
 }
