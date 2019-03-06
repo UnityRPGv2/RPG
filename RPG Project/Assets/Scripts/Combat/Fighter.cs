c3369f224f46e622f5fa8d113c0b9b44433dac64
Samuel Pattuzzi <sam@pattuzzi.it>
2019-03-06 14:34:06 +0000

    12 Throttling Our Attacks

--- RPG Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -7,11 +7,15 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
-        Transform target; 
+        Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -27,7 +31,11 @@ namespace RPG.Combat
 
         private void AttackBehaviour()
         {
-            GetComponent<Animator>().SetTrigger("attack");
+            if (timeSinceLastAttack > timeBetweenAttacks) 
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
         }
 
         private bool GetIsInRange()
