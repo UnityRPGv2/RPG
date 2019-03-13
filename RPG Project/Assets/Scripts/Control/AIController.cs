b186623f441138697009946edb0d061cb6045042
Samuel Pattuzzi <sam@pattuzzi.it>
2019-03-13 17:48:25 +0000

    Finding The Player

---
+++ RPG Project/Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using RPG.Combat;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 2;
+
+        Fighter fighter;
+
+        void Start()
+        {
+            fighter = GetComponent<Fighter>();
+        }
+
+        void Update()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+            if (distanceToPlayer < chaseDistance)
+            {
+                print("Should attack");
+            }
+        }
+    }
+}
