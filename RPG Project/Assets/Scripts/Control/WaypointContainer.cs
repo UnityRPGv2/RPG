fdf632a8d13b7432a13f5771a43ef0930cffd2fb
Samuel Pattuzzi <sam@pattuzzi.it>
2019-03-15 14:51:58 +0000

    Change the public API

--- RPG Project/Assets/Scripts/Control/WaypointContainer.cs
+++ RPG Project/Assets/Scripts/Control/WaypointContainer.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class WaypointContainer : MonoBehaviour
 {
+    public Vector3 GetWaypoint(int index)
+    {
+        return transform.GetChild(index).position;
+    }
+
+    public float GetWaypointCount()
+    {
+        return transform.childCount;
+    }
+
     private void OnDrawGizmos() {
-        for (int currentIndex = 0; currentIndex < transform.childCount; currentIndex++)
+        for (int currentIndex = 0; currentIndex < GetWaypointCount(); currentIndex++)
         {
             int nextIndex = currentIndex + 1;
-            if (nextIndex >= transform.childCount)
+            if (nextIndex >= GetWaypointCount())
             {
                 nextIndex = 0;
             }
-            Vector3 currentPos = transform.GetChild(currentIndex).position;
-            Vector3 nextPos = transform.GetChild(nextIndex).position;
+            Vector3 currentPos = GetWaypoint(currentIndex);
+            Vector3 nextPos = GetWaypoint(nextIndex);
             Gizmos.DrawLine(nextPos, currentPos);
             Gizmos.DrawSphere(currentPos, 0.3f);
         }
