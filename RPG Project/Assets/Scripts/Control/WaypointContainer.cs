6ce162032be2debc8e8e46ed9217007eefb47835
Samuel Pattuzzi <sam@pattuzzi.it>
2019-03-15 14:31:36 +0000

    Making Waypoints

---
+++ RPG Project/Assets/Scripts/Control/WaypointContainer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointContainer : MonoBehaviour
+{
+    private void OnDrawGizmos() {
+        foreach (Transform waypoint in GetWaypoints())
+        {
+            Gizmos.DrawSphere(waypoint.position, 0.3f);
+        }
+    }
+
+    Transform[] GetWaypoints()
+    {
+        return GetComponentsInChildren<Transform>();
+    }
+}
