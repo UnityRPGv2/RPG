4ba21e902595e5706fb3a5bafdee789b1af6646a
Samuel Pattuzzi <sam@pattuzzi.it>
2019-03-20 14:21:35 +0000

    9 Waypoint Loops

--- RPG Project/Assets/Scripts/Control/PatrolPath.cs
+++ RPG Project/Assets/Scripts/Control/PatrolPath.cs
@@ -11,8 +11,24 @@ namespace RPG.Control
         private void OnDrawGizmos() {
             for (int i = 0; i < transform.childCount; i++)
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
