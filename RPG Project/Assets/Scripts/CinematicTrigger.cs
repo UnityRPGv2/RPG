f64258944f04f741b59adea20b5282b3c4ab1787
Rick <rickinaction@gmail.com>
2019-03-22 16:08:05 +1000

    Trigger timeline with cameras and cancel current action. Not yet disabling player control.

---
+++ RPG Project/Assets/Scripts/CinematicTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Control;
+using RPG.Core;
+
+public class CinematicTrigger : MonoBehaviour
+{
+    [SerializeField] PlayerController PlayerController;
+    [SerializeField] ActionScheduler actionScheduler;
+
+    
+    private void OnTriggerEnter(Collider other) 
+    {
+        print("triggered");
+        if(other.GetComponent<PlayerController>())
+        {
+            GetComponent<PlayableDirector>().Play();
+            actionScheduler.CancelCurrentAction();
+            
+        }
+    }
+}
