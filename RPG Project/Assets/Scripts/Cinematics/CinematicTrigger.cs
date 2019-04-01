db30804163ead9dcf159d7f20040396dcaf2f466
Rick <rickinaction@gmail.com>
2019-04-01 14:09:32 +1000

    10 Trigger Cinemachine Cut Scene

---
+++ RPG Project/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool alreadyTriggered = false;
+        
+        private void OnTriggerEnter(Collider other) 
+        {
+            if(!alreadyTriggered && other.gameObject.tag == "Player")
+            {
+                alreadyTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+        }
+    }
+}
