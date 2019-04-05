e73c7991926c97922e03cc0133311c137ae11678
Samuel Pattuzzi <sam@pattuzzi.it>
2019-04-05 11:24:16 +0100

    6 Player Spawn Point

--- RPG Project/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG Project/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,6 +8,7 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other) {
             if (other.tag == "Player")
@@ -19,8 +21,30 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene Loaded");
+            
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
         }
     }
 }
