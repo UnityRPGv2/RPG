16ec179ba99aced5b8e19d59c173001cc4ab3237
Samuel Pattuzzi <sam@pattuzzi.it>
2019-04-09 11:44:08 +0100

    Loading from files.

--- RPG Project/Assets/Scripts/Saving/SaveSystem.cs
+++ RPG Project/Assets/Scripts/Saving/SaveSystem.cs
@@ -10,22 +10,28 @@ namespace RPG.Saving
         {
             string path = GetPathFromSaveFile(saveFile);
             print("Save: " + path);
-            FileStream stream = File.Open(path, FileMode.Create);
-            // Challenge: write hello world;
-            // stream.WriteByte(102);
-            byte[] messageBytes = Encoding.UTF8.GetBytes("Hello world");
-            stream.Write(messageBytes, 0, messageBytes.Length);
-            stream.Close();
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                byte[] messageBytes = Encoding.UTF8.GetBytes("Hello world");
+                stream.Write(messageBytes, 0, messageBytes.Length);
+            }
         }
 
         public void Load(string saveFile)
         {
-            print("Load: " + GetPathFromSaveFile(saveFile));
+            string path = GetPathFromSaveFile(saveFile);
+            print("Load: " + path);
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+                //CHALLENGE convert to text:
+                print(Encoding.UTF8.GetString(buffer));
+            }
         }
 
         private string GetPathFromSaveFile(string saveFile)
         {
-            // CHALLENGE: show docs and ask to work out.
             return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
         }
     }
