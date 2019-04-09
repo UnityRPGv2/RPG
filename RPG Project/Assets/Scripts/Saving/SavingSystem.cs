99f3552e0767934ca98e2beca2ee41a318d27c65
Samuel Pattuzzi <sam@pattuzzi.it>
2019-04-09 21:15:23 +0100

    3 File Paths In C#

--- RPG Project/Assets/Scripts/Saving/SavingSystem.cs
+++ RPG Project/Assets/Scripts/Saving/SavingSystem.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 
 namespace RPG.Saving
@@ -6,12 +7,17 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
-            print("Saving to " + saveFile);
+            print("Saving to " + GetPathFromSaveFile(saveFile));
         }
 
         public void Load(string saveFile)
         {
-            print("Loading from " + saveFile);
+            print("Loading from " + GetPathFromSaveFile(saveFile));
+        }
+
+        private string GetPathFromSaveFile(string saveFile)
+        {
+            return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
         }
     }
 }
