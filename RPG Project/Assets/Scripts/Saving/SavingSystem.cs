84cc5a892e1bb32af60a64dd110154040c62e921
Samuel Pattuzzi <sam@pattuzzi.it>
2019-04-10 16:14:18 +0100

    5 Filestream Read And Write In C#

--- RPG Project/Assets/Scripts/Saving/SavingSystem.cs
+++ RPG Project/Assets/Scripts/Saving/SavingSystem.cs
@@ -10,15 +10,24 @@ namespace RPG.Saving
         {
             string path = GetPathFromSaveFile(saveFile);
             print("Saving to " + path);
-            FileStream stream = File.Open(path, FileMode.Create);
-            byte[] bytes = Encoding.UTF8.GetBytes("¡Hola Mundo!");
-            stream.Write(bytes, 0, bytes.Length);
-            stream.Close();
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes("¡Hola Mundo!");
+                stream.Write(bytes, 0, bytes.Length);
+            }
         }
 
         public void Load(string saveFile)
         {
-            print("Loading from " + GetPathFromSaveFile(saveFile));
+            string path = GetPathFromSaveFile(saveFile);
+            print("Loading from " + path);
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+
+                print(Encoding.UTF8.GetString(buffer));
+            }
         }
 
         private string GetPathFromSaveFile(string saveFile)
