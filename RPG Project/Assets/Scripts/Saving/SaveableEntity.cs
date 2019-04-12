e5244cb0cca116e081eed0eba125ffb82d8ed125
Samuel Pattuzzi <sam@pattuzzi.it>
2019-04-12 15:31:01 +0100

    9 Generating UUIDs In C#

--- RPG Project/Assets/Scripts/Saving/SaveableEntity.cs
+++ RPG Project/Assets/Scripts/Saving/SaveableEntity.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 namespace RPG.Saving
 {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        [SerializeField] string uniqueIdentifier = "";
+
         public string GetUniqueIdentifier()
         {
-            return "";
+            return uniqueIdentifier;
         }
 
         public object CaptureState()
@@ -18,6 +21,12 @@ namespace RPG.Saving
         public void RestoreState(object state)
         {
             print("Restoring state for " + GetUniqueIdentifier());
+        }
+
+        private void Update() {
+            if (Application.IsPlaying(gameObject)) return;
+
+            print("Editing");
         }
     }
 }
