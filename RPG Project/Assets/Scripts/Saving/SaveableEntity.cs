19fa04023b7c3981f5b1def0d39ddd1b9e563189
Samuel Pattuzzi <sam@pattuzzi.it>
2019-04-16 10:38:13 +0100

    11 Serializing With SaveableEntities

--- RPG Project/Assets/Scripts/Saving/SaveableEntity.cs
+++ RPG Project/Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,5 +1,7 @@
+using RPG.Core;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RPG.Saving
 {
@@ -15,13 +17,16 @@ namespace RPG.Saving
 
         public object CaptureState()
         {
-            print("Capturing state for " + GetUniqueIdentifier());
-            return null;
+            return new SerializableVector3(transform.position);
         }
 
         public void RestoreState(object state)
         {
-            print("Restoring state for " + GetUniqueIdentifier());
+            SerializableVector3 position = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = position.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<ActionScheduler>().CancelCurrentAction();
         }
 
         private void Update() {
