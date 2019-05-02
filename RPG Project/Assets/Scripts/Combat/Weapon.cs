32c146c4de18c27f0f404390254e333c03c284fb
Samuel Pattuzzi <sam@pattuzzi.it>
2019-05-02 17:33:45 +0100

    Weapon scriptable object.

---
+++ RPG Project/Assets/Scripts/Combat/Weapon.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "RPG Project/Weapon", order = 0)]
+    public class Weapon : ScriptableObject
+    {
+        [SerializeField] AnimatorOverrideController weaponOverride = null;
+        [SerializeField] GameObject weaponPrefab = null;
+
+        public void Spawn(Transform handTransform, Animator animator)
+        {
+            Instantiate(weaponPrefab, handTransform);
+            animator.runtimeAnimatorController = weaponOverride;
+        }
+    }
+}
