97cd0966b623c48faaa59c516759fdb3a58e6ac2
Samuel Pattuzzi <sam@pattuzzi.it>
2019-05-03 07:40:12 +0100

    Move other weapon stats over

--- RPG Project/Assets/Scripts/Combat/Weapon.cs
+++ RPG Project/Assets/Scripts/Combat/Weapon.cs
@@ -5,13 +5,25 @@ namespace RPG.Combat
     [CreateAssetMenu(fileName = "Weapon", menuName = "RPG Project/Weapon", order = 0)]
     public class Weapon : ScriptableObject
     {
-        [SerializeField] AnimatorOverrideController weaponOverride = null;
-        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] AnimatorOverrideController animatorOverride = null;
+        [SerializeField] GameObject equippedPrefab = null;
+        [SerializeField] float damage = 3f;
+        [SerializeField] float range = 3f;
 
         public void Spawn(Transform handTransform, Animator animator)
         {
-            Instantiate(weaponPrefab, handTransform);
-            animator.runtimeAnimatorController = weaponOverride;
+            Instantiate(equippedPrefab, handTransform);
+            animator.runtimeAnimatorController = animatorOverride;
+        }
+
+        public float GetDamage()
+        {
+            return damage;
+        }
+
+        public float GetRange()
+        {
+            return range;
         }
     }
 }
