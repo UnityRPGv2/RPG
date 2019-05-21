74de06ce0c1f84d40c03984c6447fb3269d9a461
Rick <rickinaction@gmail.com>
2019-05-21 19:46:03 +1000

    17 Destroy The Unwanted

--- RPG Project/Assets/Scripts/Combat/Projectile.cs
+++ RPG Project/Assets/Scripts/Combat/Projectile.cs
@@ -3,57 +3,75 @@ using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+namespace RPG.Combat
 {
-    [SerializeField] float speed = 1;
-    [SerializeField] bool isHoming = true;
-    [SerializeField] GameObject hitEffect = null;
-    Health target = null;
-    float damage = 0;
-
-    private void Start() 
+    public class Projectile : MonoBehaviour
     {
-        transform.LookAt(GetAimLocation());
-    }
+        [SerializeField] float speed = 1;
+        [SerializeField] bool isHoming = true;
+        [SerializeField] GameObject hitEffect = null;
+        [SerializeField] float maxLifeTime = 10;
+        [SerializeField] GameObject[] destroyOnHit = null;
+        [SerializeField] float lifeAfterImpact = 2;
 
-    void Update()
-    {
-        if (target == null) return;
-        if (isHoming && !target.IsDead())
+        Health target = null;
+        float damage = 0;
+
+        private void Start()
         {
             transform.LookAt(GetAimLocation());
         }
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);      
-    }
 
-    public void SetTarget(Health target, float damage)
-    {
-        this.target = target;
-        this.damage = damage;
-    }
-
-    private Vector3 GetAimLocation()
-    {
-        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
-        if (targetCapsule == null)
+        void Update()
         {
-            return target.transform.position;
-        }
-        return target.transform.position + Vector3.up * targetCapsule.height / 2;
-    }
-
-    private void OnTriggerEnter(Collider other) 
-    {
-        if(other.GetComponent<Health>() != target) return;  
-        if(target.IsDead()) return;
-        target.TakeDamage(damage); 
-
-        if(hitEffect != null)
-        {
-            Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            if (target == null) return;
+            if (isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
-        Destroy(gameObject); 
+        public void SetTarget(Health target, float damage)
+        {
+            this.target = target;
+            this.damage = damage;
+
+            Destroy(gameObject, maxLifeTime);
+        }
+
+        private Vector3 GetAimLocation()
+        {
+            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+            if (targetCapsule == null)
+            {
+                return target.transform.position;
+            }
+            return target.transform.position + Vector3.up * targetCapsule.height / 2;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Health>() != target) return;
+            if (target.IsDead()) return;
+            target.TakeDamage(damage);
+
+            speed = 0;
+
+            if (hitEffect != null)
+            {
+                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
+            }
+
+            foreach (GameObject toDestroy in destroyOnHit)
+            {
+                Destroy(toDestroy);
+            }
+
+            Destroy(gameObject, lifeAfterImpact);
+
+        }
+
     }
 
 }
---
+++ RPG Project/Assets/Scripts/Core/DestroyAfterEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class DestroyAfterEffect : MonoBehaviour
+    {
+
+        void Update()
+        {
+            if (!GetComponent<ParticleSystem>().IsAlive())
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
