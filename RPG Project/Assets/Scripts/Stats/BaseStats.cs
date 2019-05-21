51f790bf2ee634c40511a89d8a1ae9803df6087a
Samuel Pattuzzi <sam@pattuzzi.it>
2019-05-21 14:35:28 +0100

    3 Progression Scriptable Object

--- RPG Project/Assets/Scripts/Stats/BaseStats.cs
+++ RPG Project/Assets/Scripts/Stats/BaseStats.cs
@@ -9,5 +9,6 @@ namespace RPG.Stats
         [Range(1, 99)]
         [SerializeField] int startingLevel = 1;
         [SerializeField] CharacterClass characterClass;
+        [SerializeField] Progression progression = null;
     }
 }
---
+++ RPG Project/Assets/Scripts/Stats/Progression.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace RPG.Stats
+{
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] ProgressionCharacterClass[] characterClasses = null;
+
+        [System.Serializable]
+        class ProgressionCharacterClass
+        {
+            [SerializeField] CharacterClass characterClass;
+            [SerializeField] float[] health;
+        }
+    }
+}
