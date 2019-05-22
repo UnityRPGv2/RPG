a066b544663bf865130938f3bf5d1ce50ab810d2
Samuel Pattuzzi <sam@pattuzzi.it>
2019-05-22 08:41:28 +0100

    18 Resetting The Default Animator

--- RPG Project/Assets/Scripts/Combat/Weapon.cs
+++ RPG Project/Assets/Scripts/Combat/Weapon.cs
@@ -25,9 +25,15 @@ namespace RPG.Combat
                 GameObject weapon = Instantiate(equippedPrefab, handTransform);
                 weapon.name = weaponName;
             }
+
+            var overrideController = animator.runtimeAnimatorController as AnimatorOverrideController;
             if (animatorOverride != null)
             {
                 animator.runtimeAnimatorController = animatorOverride; 
+            }
+            else if (overrideController != null)
+            {
+                animator.runtimeAnimatorController = overrideController.runtimeAnimatorController;
             }
         }
 
