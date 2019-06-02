d756dbbc3f05ad041fd754b6f80248d41d83de5e
Samuel Pattuzzi <sam@pattuzzi.it>
2019-06-02 13:00:01 +0100

    5 Debug Health Display

--- RPG Project/Assets/Scripts/Resources/Health.cs
+++ RPG Project/Assets/Scripts/Resources/Health.cs
@@ -30,6 +30,11 @@ namespace RPG.Resources
             }
         }
 
+        public float GetPercentage()
+        {
+            return 100 * (healthPoints / GetComponent<BaseStats>().GetHealth());
+        }
+
         private void Die()
         {
             if (isDead) return;
---
+++ RPG Project/Assets/Scripts/Resources/HealthDisplay.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Resources
+{
+    public class HealthDisplay : MonoBehaviour
+    {
+        Health health;
+
+        private void Awake()
+        {
+            health = GameObject.FindWithTag("Player").GetComponent<Health>();
+        }
+
+        private void Update()
+        {
+            GetComponent<Text>().text = String.Format("{0:0}%", health.GetPercentage());
+        }
+    }
+}
