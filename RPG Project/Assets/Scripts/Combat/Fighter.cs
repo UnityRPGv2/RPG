a7a98098fea2e97bc3c5b2caadec798b3cc15bde
Samuel Pattuzzi <sam@pattuzzi.it>
2019-06-02 13:43:04 +0100

    6 Enemy Health Display

--- RPG Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -50,6 +50,11 @@ namespace RPG.Combat
             weapon.Spawn(rightHandTransform, leftHandTransform, animator);
         }
 
+        public Health GetTarget()
+        {
+            return target;
+        } 
+
         private void AttackBehaviour()
         {
             transform.LookAt(target.transform);
