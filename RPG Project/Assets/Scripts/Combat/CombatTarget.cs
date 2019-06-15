3122b295dbf7089b855773802697797cb78f669d
Samuel Pattuzzi <sam@pattuzzi.it>
2019-06-15 12:03:05 +0100

    Challenge: Migrate Combat

--- RPG Project/Assets/Scripts/Combat/CombatTarget.cs
+++ RPG Project/Assets/Scripts/Combat/CombatTarget.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
 using RPG.Resources;
+using RPG.Control;
 
 namespace RPG.Combat
 {
     [RequireComponent(typeof(Health))]
-    public class CombatTarget : MonoBehaviour
+    public class CombatTarget : MonoBehaviour, IRaycastable
     {
+        public bool HandleRaycast(PlayerController playerController)
+        {
+            Fighter playerFighter = playerController.GetComponent<Fighter>();
+            if (!playerFighter.CanAttack(gameObject))
+            {
+                return false;
+            }
 
+            if (Input.GetMouseButton(0))
+            {
+                playerFighter.Attack(gameObject);
+            }
+            return true;
+        }
     }
 }
--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -40,7 +40,6 @@ namespace RPG.Control
             if (health.IsDead()) return;
 
             if (InteractWithComponents()) return;
-            if (InteractWithCombat()) return;
             if (InteractWithMovement()) return;
             SetCursor(CursorType.None);
         }
@@ -69,29 +68,6 @@ namespace RPG.Control
                         return true;
                     }
                 }
-            }
-            return false;
-        }
-
-        private bool InteractWithCombat()
-        {
-            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-            foreach (RaycastHit hit in hits)
-            {
-                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-                if (target == null) continue;
-
-                if (!GetComponent<Fighter>().CanAttack(target.gameObject))
-                {
-                    continue;
-                }
-
-                if (Input.GetMouseButton(0))
-                {
-                    GetComponent<Fighter>().Attack(target.gameObject);
-                }
-                SetCursor(CursorType.Combat);
-                return true;
             }
             return false;
         }
