26ad44eb4b7612e28b49305fee984367b847dc6f
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-11 12:58:33 +0100

    Wrapper setup.

---
+++ RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+public class SavingWrapper : MonoBehaviour
+{
+    const string defaultSaveFile = "save";
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Load();
+        }
+    }
+
+    private void Load()
+    {
+        GetComponent<SavingSystem>().Load(defaultSaveFile);
+    }
+}
