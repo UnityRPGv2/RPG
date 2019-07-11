0f4bddde5067711cd3ef31982b136a64cf11da4e
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-11 12:09:51 +0100

    Import and overview.

---
+++ RPG Project/Assets/Scripts/Saving/SerializableVector3.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    [System.Serializable]
+    public class SerializableVector3
+    {
+        float x, y, z;
+
+        public SerializableVector3(Vector3 vector)
+        {
+            x = vector.x;
+            y = vector.y;
+            z = vector.z;
+        }
+
+        public Vector3 ToVector()
+        {
+            return new Vector3(x, y, z);
+        }
+    }
+}
