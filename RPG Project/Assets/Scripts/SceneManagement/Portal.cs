28e64ecaf688f4a4f0b79b2922dfc35a54cbddf7
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-13 17:17:40 +0100

    7 Loading The Saved Scene

--- RPG Project/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG Project/Assets/Scripts/SceneManagement/Portal.cs
@@ -51,6 +51,8 @@ namespace RPG.SceneManagement
             Portal otherPortal = GetOtherPortal();
             UpdatePlayer(otherPortal);
 
+            wrapper.Save();
+
             yield return new WaitForSeconds(fadeWaitTime);
             yield return fader.FadeIn(fadeInTime);
 
--- RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -9,6 +9,14 @@ namespace RPG.SceneManagement
     public class SavingWrapper : MonoBehaviour
     {
         const string defaultSaveFile = "save";
+        [SerializeField] float fadeInTime = 0.2f;
+
+        IEnumerator Start() {
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
+        }
 
         void Update()
         {
