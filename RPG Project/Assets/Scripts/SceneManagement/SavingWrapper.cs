8ca1103a0aaa5baf41e44aeca1d315d1d2ce5499
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-13 13:19:14 +0100

    3 Triggering Saving And Loading

---
+++ RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+    }
+}
