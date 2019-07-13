98ed69d3966bfe756d35a50c05bcad16716e1e95
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-13 14:15:01 +0100

    5 Challenge: Saveable Health

--- RPG Project/Assets/Scripts/Core/Health.cs
+++ RPG Project/Assets/Scripts/Core/Health.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Core
 {
-    public class Health : MonoBehaviour
+    public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float healthPoints = 100f;
 
@@ -29,6 +30,21 @@ namespace RPG.Core
             isDead = true;
             GetComponent<Animator>().SetTrigger("die");
             GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        public object CaptureState()
+        {
+            return healthPoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            healthPoints = (float)state;
+
+            if (healthPoints == 0)
+            {
+                Die();
+            }
         }
     }
 }
