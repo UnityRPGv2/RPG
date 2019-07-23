266e805bd6e5c6361c79263c657a8625f3e8ce5e
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-23 16:30:22 +0100

    UnityEvents With Parameters

--- RPG Project/Assets/Scripts/Resources/Health.cs
+++ RPG Project/Assets/Scripts/Resources/Health.cs
@@ -11,7 +11,10 @@ namespace RPG.Resources
     public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float regenerationPercentage = 70;
-        [SerializeField] UnityEvent takeDamage;
+        [SerializeField] TakeDamageEvent takeDamage;
+
+        [System.Serializable]
+        public class TakeDamageEvent : UnityEvent<float> {}
 
         LazyValue<float> healthPoints;
 
@@ -49,7 +52,7 @@ namespace RPG.Resources
             print(gameObject.name + " took damage: " + damage);
 
             healthPoints.value = Mathf.Max(healthPoints.value - damage, 0);
-            takeDamage.Invoke();
+            takeDamage.Invoke(damage);
             if(healthPoints.value == 0)
             {
                 Die();
