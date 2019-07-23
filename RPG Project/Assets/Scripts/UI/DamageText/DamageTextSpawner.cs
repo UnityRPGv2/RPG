934fedeb1bdde7d1ea7db713dfaa6bfd49dbb801
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-23 17:57:32 +0100

    11 Spawning Damage Text

---
+++ RPG Project/Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.UI.DamageText
+{
+    public class DamageTextSpawner : MonoBehaviour
+    {
+        [SerializeField] DamageText damageTextPrefab = null;
+
+        void Start()
+        {
+            Spawn(11);
+        }
+
+        public void Spawn(float damageAmount)
+        {
+            DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
+        }
+    }
+}
