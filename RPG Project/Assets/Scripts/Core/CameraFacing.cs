d0cf0bc0bb53d75f32d3396a30a3bf8e17e71203
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-23 15:03:30 +0100

    In Game UI

---
+++ RPG Project/Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class CameraFacing : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
