caccd53f073eaf2351b88f186483e5ab68c591c6
Samuel Pattuzzi <sam@pattuzzi.it>
2019-07-24 13:14:31 +0100

    Disable Health Bar On Death.

--- RPG Project/Assets/Scripts/Attributes/HealthBar.cs
+++ RPG Project/Assets/Scripts/Attributes/HealthBar.cs
@@ -7,11 +7,19 @@ namespace RPG.Attributes
     public class HealthBar : MonoBehaviour
     {
         [SerializeField] Health healthComponent = null;
-        [SerializeField] RectTransform background = null;
+        [SerializeField] Canvas rootCanvas = null;
         [SerializeField] RectTransform foreground = null;
 
         void Update()
         {
+            if (Mathf.Approximately(healthComponent.GetFraction(), 0)
+            ||  Mathf.Approximately(healthComponent.GetFraction(), 1)) //CHALLENGE
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
         }
     }
