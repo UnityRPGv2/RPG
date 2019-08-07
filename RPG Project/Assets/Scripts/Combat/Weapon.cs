74c0b329d5a5e5ae98905ef9385568c422aede2e
Samuel Pattuzzi <sam@pattuzzi.it>
2019-08-07 11:14:39 +0100

    24 Boom Microphone Audio Listener

--- RPG Project/Assets/Scripts/Combat/Weapon.cs
+++ RPG Project/Assets/Scripts/Combat/Weapon.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] UnityEvent onHit;
+
         public void OnHit()
         {
-            print("Weapon Hit " + gameObject.name);
+            onHit.Invoke();
         }
     }
 }
