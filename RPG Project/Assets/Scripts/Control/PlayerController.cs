788d57f2f000cc4c4372b295d4f0d7d4aca1394d
Joey <far2wise@gmail.com>
2019-09-13 21:38:25 -0400

    Revert "7 Raycasting To A NavMesh"
    This reverts commit a8026b3925b3cbef4b6a01d94dbaee7b087f1fac.

--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using RPG.Resources;
 using System;
 using UnityEngine.EventSystems;
-using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -21,7 +20,6 @@ namespace RPG.Control
         }
 
         [SerializeField] CursorMapping[] cursorMappings = null;
-        [SerializeField] float maxNavMeshProjectionDistance = 1f;
 
         private void Awake() {
             health = GetComponent<Health>();
@@ -84,35 +82,18 @@ namespace RPG.Control
 
         private bool InteractWithMovement()
         {
-            Vector3 target;
-            bool hasHit = RaycastNavMesh(out target);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
                 if (Input.GetMouseButton(0))
                 {
-                    GetComponent<Mover>().StartMoveAction(target, 1f);
+                    GetComponent<Mover>().StartMoveAction(hit.point, 1f);
                 }
                 SetCursor(CursorType.Movement);
                 return true;
             }
             return false;
-        }
-
-        private bool RaycastNavMesh(out Vector3 target)
-        {
-            target = new Vector3();
-
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
-            if (!hasHit) return false;
-
-            NavMeshHit navMeshHit;
-            bool hasCastToNavMesh = NavMesh.SamplePosition(
-                hit.point, out navMeshHit, maxNavMeshProjectionDistance, NavMesh.AllAreas);
-            if (!hasCastToNavMesh) return false;
-
-            target = navMeshHit.position;
-            return true;
         }
 
         private void SetCursor(CursorType type)
