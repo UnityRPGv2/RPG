663794f4d38ef840ebf82c467c322592ca1af869
Joey <far2wise@gmail.com>
2019-09-13 21:37:48 -0400

    Revert "18 Fixing The Fader"
    This reverts commit e4a37e97eb747ef1ca94521e82986504752ef082.

--- RPG Project/Assets/Scripts/SceneManagement/Fader.cs
+++ RPG Project/Assets/Scripts/SceneManagement/Fader.cs
@@ -6,7 +6,6 @@ namespace RPG.SceneManagement
     public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
-        Coroutine currentActiveFade = null;
 
         private void Awake() {
             canvasGroup = GetComponent<CanvasGroup>();
@@ -19,29 +18,18 @@ namespace RPG.SceneManagement
 
         public IEnumerator FadeOut(float time)
         {
-            return Fade(1, time);
+            while (canvasGroup.alpha < 1)
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
         }
 
         public IEnumerator FadeIn(float time)
         {
-            return Fade(0, time);
-        }
-
-        public IEnumerator Fade(float target, float time)
-        {
-            if (currentActiveFade != null)
+            while (canvasGroup.alpha > 0)
             {
-                StopCoroutine(currentActiveFade);
-            }
-            currentActiveFade = StartCoroutine(FadeRoutine(target, time));
-            yield return currentActiveFade;
-        }
-
-        private IEnumerator FadeRoutine(float target, float time)
-        {
-            while (!Mathf.Approximately(canvasGroup.alpha, target))
-            {
-                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / time);
+                canvasGroup.alpha -= Time.deltaTime / time;
                 yield return null;
             }
         }
