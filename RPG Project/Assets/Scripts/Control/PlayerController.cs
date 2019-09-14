9c1fefa4a59701a08c6e1d7a86d4729300442461
Joey <far2wise@gmail.com>
2019-09-13 21:38:23 -0400

    Revert "8 Calculating A NavMesh Path"
    This reverts commit 1dc17c67c94e122650f05fbde3d0bcad69946d97.

--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -22,7 +22,6 @@ namespace RPG.Control
 
         [SerializeField] CursorMapping[] cursorMappings = null;
         [SerializeField] float maxNavMeshProjectionDistance = 1f;
-        [SerializeField] float maxNavPathLength = 40f;
 
         private void Awake() {
             health = GetComponent<Health>();
@@ -113,26 +112,7 @@ namespace RPG.Control
             if (!hasCastToNavMesh) return false;
 
             target = navMeshHit.position;
-
-            NavMeshPath path = new NavMeshPath();
-            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
-            if (!hasPath) return false;
-            if (path.status != NavMeshPathStatus.PathComplete) return false;
-            if (GetPathLength(path) > maxNavPathLength) return false;
-
             return true;
-        }
-
-        private float GetPathLength(NavMeshPath path)
-        {
-            float total = 0;
-            if (path.corners.Length < 2) return total;
-            for (int i = 0; i < path.corners.Length - 1; i++)
-            {
-                total += Vector3.Distance(path.corners[i], path.corners[i + 1]);
-            }
-
-            return total;
         }
 
         private void SetCursor(CursorType type)
