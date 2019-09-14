a46395facac32e7a9a5143511e7cb42d4b01cb63
Joey <far2wise@gmail.com>
2019-09-13 21:41:29 -0400

    Revert "17 Destroy The Unwanted"
    This reverts commit 74de06ce0c1f84d40c03984c6447fb3269d9a461.

--- RPG Project/Assets/Scripts/Combat/Projectile.cs
+++ RPG Project/Assets/Scripts/Combat/Projectile.cs
@@ -3,75 +3,57 @@ using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 
-namespace RPG.Combat
+public class Projectile : MonoBehaviour
 {
-    public class Projectile : MonoBehaviour
+    [SerializeField] float speed = 1;
+    [SerializeField] bool isHoming = true;
+    [SerializeField] GameObject hitEffect = null;
+    Health target = null;
+    float damage = 0;
+
+    private void Start() 
     {
-        [SerializeField] float speed = 1;
-        [SerializeField] bool isHoming = true;
-        [SerializeField] GameObject hitEffect = null;
-        [SerializeField] float maxLifeTime = 10;
-        [SerializeField] GameObject[] destroyOnHit = null;
-        [SerializeField] float lifeAfterImpact = 2;
+        transform.LookAt(GetAimLocation());
+    }
 
-        Health target = null;
-        float damage = 0;
-
-        private void Start()
+    void Update()
+    {
+        if (target == null) return;
+        if (isHoming && !target.IsDead())
         {
             transform.LookAt(GetAimLocation());
         }
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);      
+    }
 
-        void Update()
+    public void SetTarget(Health target, float damage)
+    {
+        this.target = target;
+        this.damage = damage;
+    }
+
+    private Vector3 GetAimLocation()
+    {
+        CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
+        if (targetCapsule == null)
         {
-            if (target == null) return;
-            if (isHoming && !target.IsDead())
-            {
-                transform.LookAt(GetAimLocation());
-            }
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            return target.transform.position;
+        }
+        return target.transform.position + Vector3.up * targetCapsule.height / 2;
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.GetComponent<Health>() != target) return;  
+        if(target.IsDead()) return;
+        target.TakeDamage(damage); 
+
+        if(hitEffect != null)
+        {
+            Instantiate(hitEffect, GetAimLocation(), transform.rotation);
         }
 
-        public void SetTarget(Health target, float damage)
-        {
-            this.target = target;
-            this.damage = damage;
-
-            Destroy(gameObject, maxLifeTime);
-        }
-
-        private Vector3 GetAimLocation()
-        {
-            CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
-            if (targetCapsule == null)
-            {
-                return target.transform.position;
-            }
-            return target.transform.position + Vector3.up * targetCapsule.height / 2;
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.GetComponent<Health>() != target) return;
-            if (target.IsDead()) return;
-            target.TakeDamage(damage);
-
-            speed = 0;
-
-            if (hitEffect != null)
-            {
-                Instantiate(hitEffect, GetAimLocation(), transform.rotation);
-            }
-
-            foreach (GameObject toDestroy in destroyOnHit)
-            {
-                Destroy(toDestroy);
-            }
-
-            Destroy(gameObject, lifeAfterImpact);
-
-        }
-
+        Destroy(gameObject); 
     }
 
 }
