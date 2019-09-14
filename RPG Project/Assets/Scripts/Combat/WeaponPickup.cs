ecfcf7f4434892cf3e884ade9ddf5a30836b5584
Joey <far2wise@gmail.com>
2019-09-13 21:37:24 -0400

    Revert "25 Health Pickup Hack"
    This reverts commit f7b96417941510828b7b7dabf8a6c421bb94a2d0.

--- RPG Project/Assets/Scripts/Combat/WeaponPickup.cs
+++ RPG Project/Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using RPG.Attributes;
 using RPG.Control;
 using UnityEngine;
 
@@ -10,27 +9,19 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour, IRaycastable
     {
         [SerializeField] WeaponConfig weapon = null;
-        [SerializeField] float healthToRestore = 0;
         [SerializeField] float respawnTime = 5;
     
         private void OnTriggerEnter(Collider other) 
         {
             if (other.gameObject.tag == "Player")
             {
-                Pickup(other.gameObject);
+                Pickup(other.GetComponent<Fighter>());
             }
         }
 
-        private void Pickup(GameObject subject)
+        private void Pickup(Fighter fighter)
         {
-            if (weapon != null)
-            {
-                subject.GetComponent<Fighter>().EquipWeapon(weapon);
-            }
-            if (healthToRestore > 0)
-            {
-                subject.GetComponent<Health>().Heal(healthToRestore);
-            }
+            fighter.EquipWeapon(weapon);
             StartCoroutine(HideForSeconds(respawnTime));
         }
 
@@ -54,7 +45,7 @@ namespace RPG.Combat
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Pickup(callingController.gameObject);
+                Pickup(callingController.GetComponent<Fighter>());
             }
             return true;
         }
