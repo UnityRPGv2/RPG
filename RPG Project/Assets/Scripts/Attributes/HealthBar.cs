6eddb5f66fe24679b5593fa1d8a51e5c2d719fe6
Joey <far2wise@gmail.com>
2019-09-13 21:37:51 -0400

    Revert "17 Disabling Empty Health Bars"
    This reverts commit 42f388b7883420dd563b9ce10cc2fe4401438476.

--- RPG Project/Assets/Scripts/Attributes/HealthBar.cs
+++ RPG Project/Assets/Scripts/Attributes/HealthBar.cs
@@ -8,19 +8,10 @@ namespace RPG.Attributes
     {
         [SerializeField] Health healthComponent = null;
         [SerializeField] RectTransform foreground = null;
-        [SerializeField] Canvas rootCanvas = null;
 
 
         void Update()
         {
-            if (Mathf.Approximately(healthComponent.GetFraction(), 0)
-            ||  Mathf.Approximately(healthComponent.GetFraction(), 1))
-            {
-                rootCanvas.enabled = false;
-                return;
-            }
-
-            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
         }
     }
