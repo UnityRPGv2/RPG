0edd4e5ac16eabc558081eb2ce054312fdca9c8e
Joey <far2wise@gmail.com>
2019-09-13 21:40:45 -0400

    Revert "3 Progression Scriptable Object"
    This reverts commit 51f790bf2ee634c40511a89d8a1ae9803df6087a.

--- RPG Project/Assets/Scripts/Stats/BaseStats.cs
+++ RPG Project/Assets/Scripts/Stats/BaseStats.cs
@@ -9,6 +9,5 @@ namespace RPG.Stats
         [Range(1, 99)]
         [SerializeField] int startingLevel = 1;
         [SerializeField] CharacterClass characterClass;
-        [SerializeField] Progression progression = null;
     }
 }
