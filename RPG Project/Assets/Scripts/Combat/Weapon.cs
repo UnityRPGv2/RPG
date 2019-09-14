944053b976d262f64f62b9af9e1b9062184fa6aa
Joey <far2wise@gmail.com>
2019-09-13 21:37:28 -0400

    Revert "24 Boom Microphone Audio Listener"
    This reverts commit 74c0b329d5a5e5ae98905ef9385568c422aede2e.

--- RPG Project/Assets/Scripts/Combat/Weapon.cs
+++ RPG Project/Assets/Scripts/Combat/Weapon.cs
@@ -1,17 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
-        [SerializeField] UnityEvent onHit;
-
         public void OnHit()
         {
-            onHit.Invoke();
+            print("Weapon Hit " + gameObject.name);
         }
     }
 }
