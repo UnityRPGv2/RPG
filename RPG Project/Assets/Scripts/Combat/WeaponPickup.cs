8f0e39d325e3d4d1e850d68f0bcbf9253187b5e2
Joey <far2wise@gmail.com>
2019-09-13 21:40:57 -0400

    Revert "21 Pickup Respawning"
    This reverts commit 85e04d204f8a04d8c5f82a2a49c6165ac4fcbc2d.

--- RPG Project/Assets/Scripts/Combat/WeaponPickup.cs
+++ RPG Project/Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,31 +7,15 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
-        [SerializeField] float respawnTime = 5;
     
         private void OnTriggerEnter(Collider other) 
         {
             if (other.gameObject.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                StartCoroutine(HideForSeconds(respawnTime));
+                Destroy(gameObject);
             }
         }
 
-        private IEnumerator HideForSeconds(float seconds)
-        {
-            ShowPickup(false);
-            yield return new WaitForSeconds(seconds);
-            ShowPickup(true);
-        }
-
-        private void ShowPickup(bool shouldShow)
-        {
-            GetComponent<Collider>().enabled = shouldShow;
-            foreach (Transform child in transform)
-            {
-                child.gameObject.SetActive(shouldShow);
-            }
-        }
     }
 }
