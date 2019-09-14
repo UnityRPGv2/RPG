bc383d23505eb1a0b09256317a10daf2499fd0a4
Joey <far2wise@gmail.com>
2019-09-13 21:36:53 -0400

    Revert "33 Enemy Mob Mechanics"
    This reverts commit 194692c3a808e68e324caeb474a889b4bd02fa16.

--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -22,7 +22,6 @@ namespace RPG.Control
 
         [SerializeField] CursorMapping[] cursorMappings = null;
         [SerializeField] float maxNavMeshProjectionDistance = 1f;
-        [SerializeField] float raycastRadius = 1f;
 
         private void Awake() {
             health = GetComponent<Health>();
@@ -73,7 +72,7 @@ namespace RPG.Control
 
         RaycastHit[] RaycastAllSorted()
         {
-            RaycastHit[] hits = Physics.SphereCastAll(GetMouseRay(), raycastRadius);
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
             float[] distances = new float[hits.Length];
             for (int i = 0; i < hits.Length; i++)
             {
