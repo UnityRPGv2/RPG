078a064270defd1b93805ba7076d952c3b3e17b8
Samuel Pattuzzi <sam@pattuzzi.it>
2020-01-29 09:09:25 +0000

    2 Integrating Pickups

---
+++ RPG Project/Assets/Scripts/Control/ClickablePickup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameDevTV.Inventories;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    [RequireComponent(typeof(Pickup))]
+    public class ClickablePickup : MonoBehaviour, IRaycastable
+    {
+        Pickup pickup;
+
+        private void Awake()
+        {
+            pickup = GetComponent<Pickup>();
+        }
+
+        public CursorType GetCursorType()
+        {
+            if (pickup.CanBePickedUp())
+            {
+                return CursorType.Pickup;
+            }
+            else
+            {
+                return CursorType.FullPickup;
+            }
+        }
+
+        public bool HandleRaycast(PlayerController callingController)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                pickup.PickupItem();
+            }
+            return true;
+        }
+    }
+}
