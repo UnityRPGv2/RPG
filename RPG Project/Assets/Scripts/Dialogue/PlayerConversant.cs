6b6fd219993c219e49f39ef3098651d4583760fa
Samuel Pattuzzi <sam@pattuzzi.it>
2020-07-17 16:09:12 +0100

    Dialogue Architecture And UI

---
+++ RPG Project/Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Dialogue
+{
+    public class PlayerConversant : MonoBehaviour
+    {
+        [SerializeField] DialogueNode currentNode;
+
+        public string GetCurrentText()
+        {
+            if (currentNode == null)
+            {
+                return "";
+            }
+
+            return currentNode.GetText();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+    }
+}
---
+++ RPG Project/Assets/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Dialogue;
+using TMPro;
+
+namespace RPG.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        PlayerConversant playerConversant;
+        [SerializeField] TextMeshProUGUI mainDialogue;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
+            mainDialogue.text = playerConversant.GetCurrentText();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
