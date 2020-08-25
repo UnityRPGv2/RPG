9ef3d2737503a1c5ba885502dc6b7e60667adae9
Kerem Çebi <keremcebibs@hotmail.com>
2020-08-25 18:52:35 +0300

    Visualized Gizmos

---
+++ RPG Project/Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+
+        const float waypointGizmoRadius = 0.3f;
+        // Start is called before the first frame update
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+
+                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+                
+            }
+        }
+    }
+}
