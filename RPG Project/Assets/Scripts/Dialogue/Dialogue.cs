521e350e3eab78b0dfeee86d47ff4e4d3f1d4112
Samuel Pattuzzi <sam@pattuzzi.it>
2020-08-27 16:48:12 +0100

    3 Dialogue Scriptable Objects

---
+++ RPG Project/Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
+public class Dialogue : ScriptableObject {
+    [SerializeField]
+    DialogueNode[] nodes;
+}
---
+++ RPG Project/Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueNode
+{
+    public string uniqueID;
+    public string text;
+    public string[] children;
+}
