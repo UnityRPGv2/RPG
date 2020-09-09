8a0170559d60a065944f1ba1f62a7be7653f3217
Samuel Pattuzzi <sam@pattuzzi.it>
2020-09-09 16:24:17 +0100

    18 Linking Nodes

--- RPG Project/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ RPG Project/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -20,6 +20,8 @@ namespace RPG.Dialogue.Editor
         DialogueNode creatingNode = null;
         [NonSerialized]
         DialogueNode deletingNode = null;
+        [NonSerialized]
+        DialogueNode linkingParentNode = null;
 
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
@@ -132,6 +134,7 @@ namespace RPG.Dialogue.Editor
             {
                 deletingNode = node;
             }
+            DrawLinkButtons(node);
             if (GUILayout.Button("+"))
             {
                 creatingNode = node;
@@ -140,6 +143,42 @@ namespace RPG.Dialogue.Editor
             GUILayout.EndHorizontal();
 
             GUILayout.EndArea();
+        }
+
+        private void DrawLinkButtons(DialogueNode node)
+        {
+            if (linkingParentNode == null)
+            {
+                if (GUILayout.Button("link"))
+                {
+                    linkingParentNode = node;
+                }
+            }
+            else if (linkingParentNode == node)
+            {
+                if (GUILayout.Button("cancel"))
+                {
+                    linkingParentNode = null;
+                }
+            }
+            else if (linkingParentNode.children.Contains(node.uniqueID))
+            {
+                if (GUILayout.Button("unlink"))
+                {
+                    Undo.RecordObject(selectedDialogue, "Remove Dialogue Link");
+                    linkingParentNode.children.Remove(node.uniqueID);
+                    linkingParentNode = null;
+                }
+            }
+            else
+            {
+                if (GUILayout.Button("child"))
+                {
+                    Undo.RecordObject(selectedDialogue, "Add Dialogue Link");
+                    linkingParentNode.children.Add(node.uniqueID);
+                    linkingParentNode = null;
+                }
+            }
         }
 
         private void DrawConnections(DialogueNode node)
