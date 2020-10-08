8965ada58f65e281f1ea2bbc11f438006e6367c4
Samuel Pattuzzi <sam@pattuzzi.it>
2020-10-08 13:30:40 +0100

    6 Choosing A Random Response

--- RPG Project/Assets/Scripts/Dialogue/PlayerConversant.cs
+++ RPG Project/Assets/Scripts/Dialogue/PlayerConversant.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace RPG.Dialogue
@@ -7,15 +8,32 @@ namespace RPG.Dialogue
     public class PlayerConversant : MonoBehaviour
     {
         [SerializeField] Dialogue currentDialogue;
+        DialogueNode currentNode = null;
+
+        private void Awake() {
+            currentNode = currentDialogue.GetRootNode();
+        }
 
         public string GetText()
         {
-            if (currentDialogue == null)
+            if (currentNode == null)
             {
                 return "";
             }
 
-            return currentDialogue.GetRootNode().GetText();
+            return currentNode.GetText();
+        }
+    
+        public void Next()
+        {
+            DialogueNode[] children = currentDialogue.GetAllChildren(currentNode).ToArray();
+            int randomIndex = Random.Range(0, children.Count());
+            currentNode = children[randomIndex];
+        }
+
+        public bool HasNext()
+        {
+            return currentDialogue.GetAllChildren(currentNode).Count() > 0;
         }
     }
 }
--- RPG Project/Assets/Scripts/UI/DialogueUI.cs
+++ RPG Project/Assets/Scripts/UI/DialogueUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using RPG.Dialogue;
 using TMPro;
+using UnityEngine.UI;
 
 namespace RPG.UI
 {
@@ -10,18 +11,27 @@ namespace RPG.UI
     {
         PlayerConversant playerConversant;
         [SerializeField] TextMeshProUGUI AIText;
+        [SerializeField] Button nextButton;
 
         // Start is called before the first frame update
         void Start()
         {
             playerConversant = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConversant>();
-            AIText.text = playerConversant.GetText();
+            nextButton.onClick.AddListener(Next);
+
+            UpdateUI();
         }
 
-        // Update is called once per frame
-        void Update()
+        void Next()
         {
+            playerConversant.Next();
+            UpdateUI();
+        }
 
+        void UpdateUI()
+        {
+            AIText.text = playerConversant.GetText();
+            nextButton.gameObject.SetActive(playerConversant.HasNext());
         }
     }
 }
