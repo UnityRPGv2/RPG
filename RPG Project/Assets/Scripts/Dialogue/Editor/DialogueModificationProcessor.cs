d4e7e1e808aea8c4682c966c478c78f5d13a1877
Samuel Pattuzzi <sam@pattuzzi.it>
2020-10-08 12:57:13 +0100

    5 Nested ScriptableObject Rename Bug

---
+++ RPG Project/Assets/Scripts/Dialogue/Editor/DialogueModificationProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace RPG.Dialogue.Editor
+{
+    public class DialogueModificationProcessor : UnityEditor.AssetModificationProcessor
+    {
+        private static AssetMoveResult OnWillMoveAsset(string sourcePath, string destinationPath)
+        {
+            Dialogue dialogue = AssetDatabase.LoadMainAssetAtPath(sourcePath) as Dialogue;
+            if (dialogue == null)
+            {
+                return AssetMoveResult.DidNotMove;
+            }
+
+            if (Path.GetDirectoryName(sourcePath) != Path.GetDirectoryName(destinationPath))
+            {
+                return AssetMoveResult.DidNotMove;
+            }
+
+            dialogue.name = Path.GetFileNameWithoutExtension(destinationPath);
+
+            return AssetMoveResult.DidNotMove;
+        }
+    }
+}
