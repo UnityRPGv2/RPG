36c145d2b50acec380dbf0eba9451833c988bf64
Samuel Pattuzzi <sam@pattuzzi.it>
2020-10-14 17:06:12 +0100

    16 Enemy Aggro Groups

---
+++ RPG Project/Assets/Scripts/Combat/AggroGroup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class AggroGroup : MonoBehaviour
+    {
+        [SerializeField] Fighter[] fighters;
+        [SerializeField] bool activateOnStart = false;
+
+        private void Start() {
+            Activate(activateOnStart);
+        }
+
+        public void Activate(bool shouldActivate)
+        {
+            foreach (Fighter fighter in fighters)
+            {
+                CombatTarget target = fighter.GetComponent<CombatTarget>();
+                if (target != null)
+                {
+                    target.enabled = shouldActivate;
+                }
+                fighter.enabled = shouldActivate;
+            }
+        }
+    }
+}
