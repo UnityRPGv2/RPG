b58d39a75123d00fb0425e35ab9bcaec2f1fcdab
Samuel Pattuzzi <sam@pattuzzi.it>
2021-03-12 15:32:18 +0000

    11 Configuring Stock And Price

--- RPG Project/Assets/Asset Packs/GameDev.tv Assets/Scripts/Inventories/InventoryItem.cs
+++ RPG Project/Assets/Asset Packs/GameDev.tv Assets/Scripts/Inventories/InventoryItem.cs
@@ -27,6 +27,7 @@ namespace GameDevTV.Inventories
         [SerializeField] Pickup pickup = null;
         [Tooltip("If true, multiple items of this type can be stacked in the same inventory slot.")]
         [SerializeField] bool stackable = false;
+        [SerializeField] float price;
 
         // STATE
         static Dictionary<string, InventoryItem> itemLookupCache;
@@ -101,6 +102,11 @@ namespace GameDevTV.Inventories
         public string GetDescription()
         {
             return description;
+        }
+
+        public float GetPrice()
+        {
+            return price;
         }
 
         // PRIVATE
--- RPG Project/Assets/Scripts/Shops/Shop.cs
+++ RPG Project/Assets/Scripts/Shops/Shop.cs
@@ -11,18 +11,32 @@ namespace RPG.Shops
     {
         [SerializeField] string shopName;
 
+        // Stock Config
+            // Item: 
+                // InventoryItem
+                // Initial Stock
+                // buyingDiscount
+        [SerializeField]
+        StockItemConfig[] stockConfig;
+
+        [System.Serializable]
+        class StockItemConfig
+        {
+            public InventoryItem item;
+            public int initialStock;
+            [Range(0, 100)]
+            public float buyingDiscountPercentage;
+        }
+
         public event Action onChange;
 
         public IEnumerable<ShopItem> GetFilteredItems() 
         {
-            yield return new ShopItem(InventoryItem.GetFromID("e75a0c32-d41c-4651-8496-92cb958a8f1e"),
-                10, 10.0f, 0);
-            yield return new ShopItem(InventoryItem.GetFromID("dbc1e40e-d3bd-4e26-a62b-6cff0e46c415"),
-                10, 10.0f, 0);
-            yield return new ShopItem(InventoryItem.GetFromID("77ad666c-513c-4c88-87b2-c8594bcd5a89"),
-                10, 1000.99f, 0);
-            yield return new ShopItem(InventoryItem.GetFromID("77ad666c-513c-4c88-87b2-c8594bcd5a89"),
-                10, 10.0f, 0);
+            foreach (StockItemConfig config in stockConfig)
+            {
+                float price = config.item.GetPrice() * (1 - config.buyingDiscountPercentage/100);
+                yield return new ShopItem(config.item, config.initialStock, price, 0);
+            }
         }
 
         public void SelectFilter(ItemCategory category) {}
