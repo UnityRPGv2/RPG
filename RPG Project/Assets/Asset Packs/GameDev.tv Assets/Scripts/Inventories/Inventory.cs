4ccfb551733d34a5ab6089c3dbc262c007076559
Samuel Pattuzzi <sam@pattuzzi.it>
2021-03-25 11:41:51 +0000

    21 Stackables And Free Slots

--- RPG Project/Assets/Asset Packs/GameDev.tv Assets/Scripts/Inventories/Inventory.cs
+++ RPG Project/Assets/Asset Packs/GameDev.tv Assets/Scripts/Inventories/Inventory.cs
@@ -53,12 +53,34 @@ namespace GameDevTV.Inventories
 
         public bool HasSpaceFor(IEnumerable<InventoryItem> items)
         {
-            int count = 0;
+            int freeSlots = FreeSlots();
+            List<InventoryItem> stackedItems = new List<InventoryItem>();
             foreach (var item in items)
             {
-                count++;
+                if (item.IsStackable())
+                {
+                    if (HasItem(item)) continue;
+                    if (stackedItems.Contains(item)) continue;
+                    stackedItems.Add(item);
+                }
+                // Already seen in the list
+                if (freeSlots <= 0) return false;
+                freeSlots--;
             }
-            return count <= inventorySize;
+            return true;
+        }
+
+        public int FreeSlots()
+        {
+            int count = 0;
+            foreach (InventorySlot slot in slots)
+            {
+                if (slot.number == 0)
+                {
+                    count ++;
+                }
+            }
+            return count;
         }
 
         /// <summary>
