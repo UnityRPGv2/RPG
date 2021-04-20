162cc377e02a1e189dc3f0dade7e0f56f0fb0d36
Samuel Pattuzzi <sam@pattuzzi.it>
2021-04-20 12:30:27 +0100

    Add Targetting Prefab

--- RPG Project/Assets/Scripts/Abilities/Targeting/DelayedClickTargeting.cs
+++ RPG Project/Assets/Scripts/Abilities/Targeting/DelayedClickTargeting.cs
@@ -11,10 +11,12 @@ namespace RPG.Abilities.Targeting
     {
         [SerializeField] Texture2D effectCursor;
         [SerializeField] Vector2 cursorHotspot;
-        [SerializeField] float areaOfEffect;
+        [SerializeField] float areaOfEffectRadius;
         [SerializeField] LayerMask castingLayer;
+        [SerializeField] Transform targettingCirclePrefab;
 
         PlayerController playerController;
+        Transform targettingCircle;
 
         public override void StartTargeting(GameObject user, Action<IEnumerable<GameObject>> confirm)
         {
@@ -25,11 +27,27 @@ namespace RPG.Abilities.Targeting
         private IEnumerator Targeting(GameObject user, Action<IEnumerable<GameObject>> confirm)
         {
             playerController.enabled = false;
+            if (!targettingCircle) targettingCircle = Instantiate(targettingCirclePrefab);
+            
             while (true)
             {
                 Cursor.SetCursor(effectCursor, cursorHotspot, CursorMode.Auto);
+
+                RaycastHit mouseHit;
+                if (Physics.Raycast(PlayerController.GetMouseRay(), out mouseHit, 100, castingLayer))
+                {
+                    targettingCircle.gameObject.SetActive(true);
+                    targettingCircle.position = mouseHit.point;
+                    targettingCircle.localScale = new Vector3(areaOfEffectRadius*2, 1, areaOfEffectRadius*2);
+                }
+                else
+                {
+                    targettingCircle.gameObject.SetActive(false);
+                }
+
                 if (Input.GetMouseButtonDown(0))
                 {
+                    targettingCircle.gameObject.SetActive(false);
                     if (confirm != null) confirm(GetGameObjectsInArea());
                     // Capture the whole of this mouse click so we don't move.
                     yield return new WaitWhile(() => Input.GetMouseButton(0));
@@ -45,7 +63,7 @@ namespace RPG.Abilities.Targeting
             RaycastHit mouseHit;
             if (Physics.Raycast(PlayerController.GetMouseRay(), out mouseHit, 100, castingLayer))
             {
-                RaycastHit[] hits = Physics.SphereCastAll(mouseHit.point, areaOfEffect, Vector3.up, 0);
+                RaycastHit[] hits = Physics.SphereCastAll(mouseHit.point, areaOfEffectRadius, Vector3.up, 0);
                 foreach (RaycastHit hit in hits)
                 {
                     yield return hit.transform.gameObject;
