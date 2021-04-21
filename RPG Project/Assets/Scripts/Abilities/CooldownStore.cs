29abad2379760e321d65123477f89063f41bda58
Samuel Pattuzzi <sam@pattuzzi.it>
2021-04-21 13:54:42 +0100

    Cool Down UI

--- RPG Project/Assets/Scripts/Abilities/CooldownStore.cs
+++ RPG Project/Assets/Scripts/Abilities/CooldownStore.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using GameDevTV.Inventories;
@@ -8,23 +9,36 @@ namespace RPG.Abilities
     public class CooldownStore : MonoBehaviour
     {
         Dictionary<InventoryItem, float> timers = new Dictionary<InventoryItem, float>();
+        Dictionary<InventoryItem, float> initialTime = new Dictionary<InventoryItem, float>();
 
         public void StartCooldown(InventoryItem item, float time)
         {
+            initialTime[item] = time;
             timers[item] = Time.time + time;
-            Debug.Log(timers);
         }
 
         public float GetTimeRemaining(InventoryItem item)
         {
+            if (item == null) return 0;
             if (!timers.ContainsKey(item)) return 0;
             float remaining = timers[item] - Time.time;
             if (remaining < 0)
             {
                 remaining = 0;
                 timers.Remove(item);
+                initialTime.Remove(item);
             }
             return remaining;
+        }
+
+        public float GetFractionRemaining(InventoryItem item)
+        {
+            float remainingTime = GetTimeRemaining(item);
+            if (remainingTime <= 0)
+            {
+                return 0;
+            }
+            return remainingTime / initialTime[item];
         }
     }
 }
