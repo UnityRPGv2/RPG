d26da0f0059f38f350185aa2ea6155452d13f0cd
Samuel Pattuzzi <sam@pattuzzi.it>
2021-04-22 11:08:44 +0100

    Regenerating Mana

--- RPG Project/Assets/Scripts/Attributes/Mana.cs
+++ RPG Project/Assets/Scripts/Attributes/Mana.cs
@@ -8,10 +8,27 @@ namespace RPG.Attributes
     public class Mana : MonoBehaviour
     {
         [SerializeField] float maxMana = 100;
+        [SerializeField] float regenerationRate = 5;
         float mana;
 
         private void Awake() {
             mana = maxMana;
+        }
+
+        private void Update() {
+            if (mana < maxMana)
+            {
+                RegenerateMana(regenerationRate * Time.deltaTime);
+            }
+        }
+
+        public void RegenerateMana(float points)
+        {
+            mana += points;
+            if (mana > maxMana)
+            {
+                mana = maxMana;
+            }
         }
 
         public float GetMana()
