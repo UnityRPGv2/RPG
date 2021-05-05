ed76975951e3ff288f5414ba4cf0c105db1d5c60
Samuel Pattuzzi <sam@pattuzzi.it>
2021-05-05 13:07:41 +0100

    7 Making A Summoning Circle

--- RPG Project/Assets/Scripts/Abilities/Targeting/DelayedClickTargeting.cs
+++ RPG Project/Assets/Scripts/Abilities/Targeting/DelayedClickTargeting.cs
@@ -13,6 +13,9 @@ namespace RPG.Abilities.Targeting
         [SerializeField] Vector2 cursorHotspot;
         [SerializeField] LayerMask layerMask;
         [SerializeField] float areaAffectRadius;
+        [SerializeField] Transform targetingPrefab;
+
+        Transform targetingPrefabInstance = null;
 
         public override void StartTargeting(GameObject user, Action<IEnumerable<GameObject>> finished)
         {
@@ -23,32 +26,43 @@ namespace RPG.Abilities.Targeting
         private IEnumerator Targeting(GameObject user, PlayerController playerController, Action<IEnumerable<GameObject>> finished)
         {
             playerController.enabled = false;
+            if (targetingPrefabInstance == null)
+            {
+                targetingPrefabInstance = Instantiate(targetingPrefab);
+            }
+            else
+            {
+                targetingPrefabInstance.gameObject.SetActive(true);
+            }
+            targetingPrefabInstance.localScale = new Vector3(areaAffectRadius*2, 1, areaAffectRadius*2);
             while (true)
             {
                 Cursor.SetCursor(cursorTexture, cursorHotspot, CursorMode.Auto);
-
-                if (Input.GetMouseButtonDown(0))
+                RaycastHit raycastHit;
+                if (Physics.Raycast(PlayerController.GetMouseRay(), out raycastHit, 1000, layerMask))
                 {
-                    // Absorb the whole mouse click
-                    yield return new WaitWhile(() => Input.GetMouseButton(0));
-                    playerController.enabled = true;
-                    finished(GetGameObjectsInRadius());
-                    yield break;
+                    targetingPrefabInstance.position = raycastHit.point;
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        // Absorb the whole mouse click
+                        yield return new WaitWhile(() => Input.GetMouseButton(0));
+                        playerController.enabled = true;
+                        targetingPrefabInstance.gameObject.SetActive(false);
+                        finished(GetGameObjectsInRadius(raycastHit.point));
+                        yield break;
+                    }
                 }
                 yield return null;
             }
         }
 
-        private IEnumerable<GameObject> GetGameObjectsInRadius()
+        private IEnumerable<GameObject> GetGameObjectsInRadius(Vector3 point)
         {
-            RaycastHit raycastHit;
-            if (Physics.Raycast(PlayerController.GetMouseRay(), out raycastHit, 1000, layerMask))
+            RaycastHit[] hits = Physics.SphereCastAll(point, areaAffectRadius, Vector3.up, 0);
+            foreach (var hit in hits)
             {
-                RaycastHit[] hits = Physics.SphereCastAll(raycastHit.point, areaAffectRadius, Vector3.up, 0);
-                foreach (var hit in hits)
-                {
-                    yield return hit.collider.gameObject;
-                }
+                yield return hit.collider.gameObject;
             }
         }
     }
