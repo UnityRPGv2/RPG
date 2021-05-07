0074efd6e93ca12955445cbf165232f17d958d96
Samuel Pattuzzi <sam@pattuzzi.it>
2021-05-07 09:40:55 +0100

    18 The Composite Pattern

---
+++ RPG Project/Assets/Scripts/Abilities/Effects/DelayCompositeEffect.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Abilities.Effects
+{
+    [CreateAssetMenu(fileName = "Delay Composite Effect", menuName = "Abilities/Effects/Delay Composite", order = 0)]
+    public class DelayCompositeEffect : EffectStrategy
+    {
+        [SerializeField] float delay = 0;
+        [SerializeField] EffectStrategy[] delayedEffects;
+
+        public override void StartEffect(AbilityData data, Action finished)
+        {
+            data.StartCoroutine(DelayedEffect(data, finished));
+        }
+
+        private IEnumerator DelayedEffect(AbilityData data, Action finished)
+        {
+            yield return new WaitForSeconds(delay);
+            foreach (var effect in delayedEffects)
+            {
+                effect.StartEffect(data, finished);
+            }
+        }
+    }
+}
