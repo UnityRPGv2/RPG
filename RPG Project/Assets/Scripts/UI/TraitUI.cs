41dd9a86977d379d811bdef0b139a3f996ad4f3a
Samuel Pattuzzi <sam@pattuzzi.it>
2021-05-10 15:26:34 +0100

    Unassigned Points UI

---
+++ RPG Project/Assets/Scripts/UI/TraitUI.cs
@@ -0,0 +1,25 @@
+using RPG.Stats;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.UI
+{
+    public class TraitUI : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI unassignedPointsText;
+        [SerializeField] Button commitButton;
+        
+        Traits playerTraits = null;
+
+        private void Awake()
+        {
+            playerTraits = GameObject.FindGameObjectWithTag("Player").GetComponent<Traits>();
+        }
+
+
+        private void Update() {
+            unassignedPointsText.text = playerTraits.GetUnassigned().ToString();
+        }
+    }
+}
