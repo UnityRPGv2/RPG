6c907b63693f9fdb4a1bd89bfc7e7c01a5464c3b
Samuel Pattuzzi <sam@pattuzzi.it>
2021-05-12 15:17:44 +0100

    7 Traits And Modifiers

--- RPG Project/Assets/Scripts/Stats/TraitStore.cs
+++ RPG Project/Assets/Scripts/Stats/TraitStore.cs
@@ -1,13 +1,46 @@
 using System;
 using System.Collections.Generic;
+using GameDevTV.Saving;
 using UnityEngine;
 
 namespace RPG.Stats
 {
-    public class TraitStore : MonoBehaviour
+    public class TraitStore : MonoBehaviour, IModifierProvider, ISaveable
     {
+        [SerializeField] TraitBonus[] bonusConfig;
+        [System.Serializable]
+        class TraitBonus
+        {
+            public Trait trait;
+            public Stat stat;
+            public float additiveBonusPerPoint = 0;
+            public float percentageBonusPerPoint = 0;
+        }
+
         Dictionary<Trait, int> assignedPoints = new Dictionary<Trait, int>();
         Dictionary<Trait, int> stagedPoints = new Dictionary<Trait, int>();
+
+        Dictionary<Stat, Dictionary<Trait, float>> additiveBonusCache;
+        Dictionary<Stat, Dictionary<Trait, float>> percentageBonusCache;
+
+        private void Awake()
+        {
+            additiveBonusCache = new Dictionary<Stat, Dictionary<Trait, float>>();
+            percentageBonusCache = new Dictionary<Stat, Dictionary<Trait, float>>();
+            foreach (var bonus in bonusConfig)
+            {
+                if (!additiveBonusCache.ContainsKey(bonus.stat))
+                {
+                    additiveBonusCache[bonus.stat] = new Dictionary<Trait, float>();
+                }
+                if (!percentageBonusCache.ContainsKey(bonus.stat))
+                {
+                    percentageBonusCache[bonus.stat] = new Dictionary<Trait, float>();
+                }
+                additiveBonusCache[bonus.stat][bonus.trait] = bonus.additiveBonusPerPoint;
+                percentageBonusCache[bonus.stat][bonus.trait] = bonus.percentageBonusPerPoint;
+            }
+        }
 
         public int GetProposedPoints(Trait trait)
         {
@@ -69,6 +102,38 @@ namespace RPG.Stats
         public int GetAssignablePoints()
         {
             return (int)GetComponent<BaseStats>().GetStat(Stat.TotalTraitPoints);
+        }
+
+        public IEnumerable<float> GetAdditiveModifiers(Stat stat)
+        {
+            if (!additiveBonusCache.ContainsKey(stat)) yield break;
+
+            foreach (Trait trait in additiveBonusCache[stat].Keys)
+            {
+                float bonus = additiveBonusCache[stat][trait];
+                yield return bonus * GetPoints(trait);
+            }
+        }
+
+        public IEnumerable<float> GetPercentageModifiers(Stat stat)
+        {
+            if (!percentageBonusCache.ContainsKey(stat)) yield break;
+
+            foreach (Trait trait in percentageBonusCache[stat].Keys)
+            {
+                float bonus = percentageBonusCache[stat][trait];
+                yield return bonus * GetPoints(trait);
+            }
+        }
+
+        public object CaptureState()
+        {
+            return assignedPoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            assignedPoints = new Dictionary<Trait, int>((IDictionary<Trait, int>)state);
         }
     }
 }
