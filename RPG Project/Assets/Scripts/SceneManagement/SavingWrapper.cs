3bad8e3538a72884a52941e7a5efd83fde5ca0db
Samuel Pattuzzi <sam@pattuzzi.it>
2021-05-17 17:19:54 +0100

    6 New Game & PlayerPrefs

--- RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -1,25 +1,52 @@
+using System;
 using System.Collections;
 using GameDevTV.Saving;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
-        const string defaultSaveFile = "save";
-
+        private const string currentSaveKey = "currentSaveName";
         [SerializeField] float fadeInTime = 0.2f;
         [SerializeField] float fadeOutTime = 0.2f;
+        [SerializeField] int firstFieldBuildIndex = 1;
 
         public void ContinueGame() 
         {
             StartCoroutine(LoadLastScene());
         }
 
-        private IEnumerator LoadLastScene() {
+        public void NewGame(string saveFile)
+        {
+            SetCurrentSave(saveFile);
+            StartCoroutine(LoadFirstScene());
+        }
+
+        private void SetCurrentSave(string saveFile)
+        {
+            PlayerPrefs.SetString(currentSaveKey, saveFile);
+        }
+
+        private string GetCurrentSave()
+        {
+            return PlayerPrefs.GetString(currentSaveKey);
+        }
+
+        private IEnumerator LoadLastScene()
+        {
             Fader fader = FindObjectOfType<Fader>();
             yield return fader.FadeOut(fadeOutTime);
-            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return GetComponent<SavingSystem>().LoadLastScene(GetCurrentSave());
+            yield return fader.FadeIn(fadeInTime);
+        }
+
+        private IEnumerator LoadFirstScene()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(firstFieldBuildIndex);
             yield return fader.FadeIn(fadeInTime);
         }
 
@@ -40,17 +67,17 @@ namespace RPG.SceneManagement
 
         public void Load()
         {
-            GetComponent<SavingSystem>().Load(defaultSaveFile);
+            GetComponent<SavingSystem>().Load(GetCurrentSave());
         }
 
         public void Save()
         {
-            GetComponent<SavingSystem>().Save(defaultSaveFile);
+            GetComponent<SavingSystem>().Save(GetCurrentSave());
         }
 
         public void Delete()
         {
-            GetComponent<SavingSystem>().Delete(defaultSaveFile);
+            GetComponent<SavingSystem>().Delete(GetCurrentSave());
         }
     }
 }
--- RPG Project/Assets/Scripts/UI/MainMenuUI.cs
+++ RPG Project/Assets/Scripts/UI/MainMenuUI.cs
@@ -2,12 +2,15 @@ using System;
 using GameDevTV.Utils;
 using RPG.SceneManagement;
 using UnityEngine;
+using TMPro;
 
 namespace RPG.UI
 {
     public class MainMenuUI : MonoBehaviour
     {
         LazyValue<SavingWrapper> savingWrapper;
+
+        [SerializeField] TMP_InputField newGameNameField;
 
         private void Awake() {
             savingWrapper = new LazyValue<SavingWrapper>(GetSavingWrapper);
@@ -21,6 +24,11 @@ namespace RPG.UI
         public void ContinueGame()
         {
             savingWrapper.value.ContinueGame();
+        }
+
+        public void NewGame()
+        {
+            savingWrapper.value.NewGame(newGameNameField.text);
         }
     }
 }
