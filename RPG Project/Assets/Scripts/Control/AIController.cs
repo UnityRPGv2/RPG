04c03d166936bfac9f61cfbe2ca873532faef251
Samuel Pattuzzi <sam@pattuzzi.it>
2021-05-18 16:20:47 +0100

    13 Resetting Enemies

--- RPG Project/Assets/Scripts/Control/AIController.cs
+++ RPG Project/Assets/Scripts/Control/AIController.cs
@@ -7,6 +7,7 @@ using RPG.Movement;
 using UnityEngine;
 using RPG.Attributes;
 using GameDevTV.Utils;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -40,6 +41,17 @@ namespace RPG.Control
             player = GameObject.FindWithTag("Player");
 
             guardPosition = new LazyValue<Vector3>(GetGuardPosition);
+            guardPosition.ForceInit();
+        }
+
+        public void Reset()
+        {
+            NavMeshAgent navMeshAgent = GetComponent<NavMeshAgent>();
+            navMeshAgent.Warp(guardPosition.value);
+            timeSinceLastSawPlayer = Mathf.Infinity;
+            timeSinceArrivedAtWaypoint = Mathf.Infinity;
+            timeSinceAggrevated = Mathf.Infinity;
+            currentWaypointIndex = 0;
         }
 
         private Vector3 GetGuardPosition()
@@ -48,7 +60,6 @@ namespace RPG.Control
         }
 
         private void Start() {
-            guardPosition.ForceInit();
         }
 
         private void Update()
--- RPG Project/Assets/Scripts/Control/Respawner.cs
+++ RPG Project/Assets/Scripts/Control/Respawner.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using Cinemachine;
 using RPG.Attributes;
 using RPG.SceneManagement;
 using UnityEngine;
@@ -12,10 +13,18 @@ namespace RPG.Control
         [SerializeField] float respawnDelay = 3;
         [SerializeField] float fadeTime = 0.2f;
         [SerializeField] float healthRegenPercentage = 20;
+        [SerializeField] float enemyHealthRegenPercentage = 20;
 
         private void Awake()
         {
             GetComponent<Health>().onDie.AddListener(Respawn);
+        }
+
+        private void Start() {
+            if (GetComponent<Health>().IsDead())
+            {
+                Respawn();
+            }
         }
 
         private void Respawn()
@@ -25,13 +34,41 @@ namespace RPG.Control
 
         private IEnumerator RespawnRoutine()
         {
+            SavingWrapper savingWrapper = FindObjectOfType<SavingWrapper>();
+            savingWrapper.Save();
             yield return new WaitForSeconds(respawnDelay);
             Fader fader = FindObjectOfType<Fader>();
             yield return fader.FadeOut(fadeTime);
+            RespawnPlayer();
+            ResetEnemies();
+            savingWrapper.Save();
+            yield return fader.FadeIn(fadeTime);
+        }
+
+        private void ResetEnemies()
+        {
+            foreach (AIController enemyControllers in FindObjectsOfType<AIController>())
+            {
+                enemyControllers.Reset();
+                Health health = enemyControllers.GetComponent<Health>();
+                if (health && !health.IsDead())
+                {
+                    health.Heal(health.GetMaxHealthPoints() * enemyHealthRegenPercentage / 100);
+                }
+            }
+        }
+
+        private void RespawnPlayer()
+        {
+            Vector3 postionDelta = respawnLocation.position - transform.position;
             GetComponent<NavMeshAgent>().Warp(respawnLocation.position);
             Health health = GetComponent<Health>();
             health.Heal(health.GetMaxHealthPoints() * healthRegenPercentage / 100);
-            yield return fader.FadeIn(fadeTime);
+            ICinemachineCamera activeVirtualCamera = FindObjectOfType<CinemachineBrain>().ActiveVirtualCamera;
+            if (activeVirtualCamera.Follow == transform)
+            {
+                activeVirtualCamera.OnTargetObjectWarped(transform, postionDelta);
+            }
         }
     }
 }
